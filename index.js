e8ecf6bcb6b0db78f30628cc29330fd795cdb16a
Matt A <mambroziak@users.noreply.github.com>
2019-12-23 22:39:27 -0500

    format cleanup

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 /*
-Lambda function for pushing events from SNS to Slack
-Modified from the Cloudwatch to Slack template and adopted for Dome9  1.19.2017
+# *******************************************************************************
+# Name: index.js (Dome9 Compliance & Governance findings pushed to Slack)
+# Description: AWS Lambda function which consumes Dome9 Compliance findings via 
+# SNS, pretty formats, and pushes to the defined slack channel. Filter for 
+# severity level is supported.
+#
+#
+# Copywrite 2019, Check Point Software
+# www.checkpoint.com
+# *******************************************************************************
 
 Variables needed:
 hookUrl - slack webhook url
 slackChannel - individual channel to post to
+severityFilter - CSV of high,medium,low
 */
 
 'use strict';
@@ -52,35 +61,48 @@ function postMessage(message, callback) {
 }
 
 function processEvent(event, callback) {
+    
+    // Prepare message
+    var message = JSON.parse(event.Records[0].Sns.Message);
+    console.log('From SNS:', message);
     // Process severity filters
     if (! severityFilter) {
         severityFilter = "high,medium,low";
     }
     var sevFilterArray = severityFilter.toLowerCase().split(',');
     if (! sevFilterArray.includes(message.rule.severity.toLowerCase()) ) {
-        callback(`Finding dropped due to severity filter. Severity levels allowed: ${severityFilter}`)
+        callback(`Finding dropped due to severity filter. Severity levels allowed: ${severityFilter}`);
     }
-    var message = JSON.parse(event.Records[0].Sns.Message);
-    // console.log('From SNS:', message);
     
     // Format message bits
+    var formatted_ruleId = '';
     if (message.rule.ruleId) {
-        var formatted_ruleId = `<https://gsl.dome9.com/${message.rule.ruleId}.html|${message.rule.ruleId}>`;
+        formatted_ruleId = `<https://gsl.dome9.com/${message.rule.ruleId}.html|${message.rule.ruleId}>`;
     }
     else {
-        var formatted_ruleId = "n/a";
+        formatted_ruleId = "n/a";
     }
     
+     var formatted_entityId = '';
     if (message.entity.type == "SecurityGroup") {
-        var formatted_entityId = `<https://secure.dome9.com/v2/security-group/${message.account.vendor.toLowerCase()}/${message.entity.id}|${message.entity.id}>`
+        formatted_entityId = `<https://secure.dome9.com/v2/security-group/${message.account.vendor.toLowerCase()}/${message.entity.id}|${message.entity.id}>`;
     }
     else {
-        var formatted_entityId = `<https://secure.dome9.com/v2/protected-asset/index?query=%7B%22filter%22:%7B%22fields%22:%5B%7B%22name%22:%22organizationalUnitId%22,%22value%22:%2200000000-0000-0000-0000-000000000000%22%7D%5D,%22freeTextPhrase%22:%22${message.entity.id}%22%7D%7D|${message.entity.id}>`
+        formatted_entityId = `<https://secure.dome9.com/v2/protected-asset/index?query=%7B%22filter%22:%7B%22fields%22:%5B%7B%22name%22:%22organizationalUnitId%22,%22value%22:%2200000000-0000-0000-0000-000000000000%22%7D%5D,%22freeTextPhrase%22:%22${message.entity.id}%22%7D%7D|${message.entity.id}>`;
     }
     
-    var formatted_account = `<https://secure.dome9.com/v2/cloud-account/${message.account.vendor.toLowerCase()}/${message.account.dome9CloudAccountId}|${message.account.name}> (${message.account.vendor} | ${message.account.id})`;
+    var formatted_account_id = '';
+    if (message.account.vendor.toLowerCase() == "aws") {
+        formatted_account_id = `<https://${message.account.id}.signin.aws.amazon.com/console/|${message.account.id}>`;
+    }
+    else {
+        formatted_account_id = message.account.id;
+    }
     
-    var formatted_message = `*Dome9 Compliance & Governance* \n${message.rule.name}\n>*ReportTime*: ${message.reportTime} \n>*Status*: ${message.status} \n>*Severity Level*: ${message.rule.severity} \n>*Region*: ${message.entity.region} \n>*Rule*: ${message.rule.name} \n>*Rule ID*: ${formatted_ruleId} \n>*Account*: ${formatted_account} \n>*Entity Type*: ${message.entity.type} \n>*Entity ID*: ${formatted_entityId}`;
+    var formatted_account = `<https://secure.dome9.com/v2/cloud-account/${message.account.vendor.toLowerCase()}/${message.account.dome9CloudAccountId}|${message.account.name}> (${message.account.vendor} | ${formatted_account_id})`;
+    
+    // Construct final message
+    var formatted_message = `*Dome9 Compliance & Governance* \n${message.rule.name}\n>*ReportTime*: ${message.reportTime} \n>*Status*: ${message.status} \n>*Severity Level*: ${message.rule.severity} \n>*Region*: ${message.region} \n>*Rule*: ${message.rule.name} \n>*Rule ID*: ${formatted_ruleId} \n>*Account*: ${formatted_account} \n>*Entity Type*: ${message.entity.type} \n>*Entity ID*: ${formatted_entityId}`;
 
     const slackMessage = {
         channel: slackChannel,
